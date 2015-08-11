d785be85e94a94e0d9bdcbe55358488e2cc7ea2c
rey832 <asif.rehan9@gmail.com>
2015-08-11 00:28:50 -0400

    static method and static variables

--- testJava/Fraction.java
+++ testJava/Fraction.java
@@ -18,7 +18,7 @@ public class Fraction extends Number implements Comparable<Fraction>{
 	}
 	public Fraction(Integer num, Integer den) {
 		this.numerator = num;
-		this.denominator = den;
+		this.denominator = den;i
 	}
 	public Fraction(Integer num){
 		this.numerator = num;
